b141a5f330fa37f8f01d14c16238a74d98211b0d
Daniel Martinez <roanhawkeye@gmail.com>
2017-02-09 11:15:21 -0600

    Calculates the actors that appears in more movies

--- src/org/roan/main/MainMoviesActors.java
+++ src/org/roan/main/MainMoviesActors.java
@@ -3,8 +3,12 @@ package org.roan.main;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -56,6 +60,23 @@ public class MainMoviesActors {
 				.count();
 		
 		System.out.println("# of actors : " + numberOfActors);
-	}
+		
+		// # of actors that played in the greatest # of movies
+		
+//		 Map<Actor, Long> collect = 
+		
+		Entry<Actor, Long> mostViewedActor = movies.stream()
+						.flatMap(movie -> movie.actors().stream()) //Stream<Actor>
+						.collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+						.entrySet().stream() // Stream<Map.Entry<Actor, Long>>
+						.max(
+								//Comparator.comparing(entry -> entry.getValue())
+								Map.Entry.comparingByValue()
+							)
+						.get();
+		
+		System.out.println("Most viewed actor: " + mostViewedActor);
+		
+	}					
 	
 }
